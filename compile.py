ab5b3ae72d1b475a3e9ba063d739ebc05500693d
ChaoticAlexander <tananikas@hotmail.com>
2024-03-19 22:04:14 +0100

    move and rename

--- compile.py
+++ compile.py
@@ -2,7 +2,7 @@ import os
 
 files = {
     "main.py": "--onefile --noconsole --icon=icons/helldivers.ico --name=stratagems --clean",
-    "./src/configure.py": "--onefile --icon=icons/settings.ico --name=settings --clean",
+    "settings.py": "--onefile --icon=icons/settings.ico --name=settings --clean",
 }
 
 for file, options in files.items():
